0fd241775bdf9463475c3f95360940748ae353b0
Roman Dolmatov <d_roman.kst@mail.ru>
2023-05-05 13:09:15 +0300

    Fixed install mongodb

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 #Install MongoDB
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
+sudo apt install apt-transport-https ca-certificates
+sudo apt update
+sudo apt install -y mongodb
 sudo systemctl enable mongod
-sudo systemctl status mongod
+sudo systemctl start mongod
